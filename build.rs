7a7dd3af487ad4e8f0d09362c9b76d5c52179136
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2025-10-21 21:14:25 +0200

    feat: add version / build information
    This is just a nice little QOL feature for users of the program.
    Lets them know what version they're running, and the git status
    at build time.

--- build.rs
+++ build.rs
@@ -1,3 +1,22 @@
-fn main() {
+use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // regenerate lalrpop parser
     lalrpop::process_root().unwrap();
+
+    // gather build info
+    let build = BuildBuilder::default().build_timestamp(true).build()?;
+    let git = GitclBuilder::default()
+        .branch(true)
+        .sha(false)
+        .dirty(false)
+        .build()?;
+
+    // emit build data into the build environment
+    Emitter::new()
+        .add_instructions(&build)?
+        .add_instructions(&git)?
+        .emit()?;
+
+    Ok(())
 }
